cb7b8981e67c8d79040cc283237799d15d2a4fa8
brianhouse <brian.house@gmail.com>
2015-06-09 19:35:40 -0400

    switched outputs

--- main_keyer.py
+++ main_keyer.py
@@ -12,10 +12,10 @@ time.sleep(1)
 ctx = animation.Context(100, 100)
 def on_mouse_press(data):
     print("ON")
-    sender.send("/noteon", 2)
+    sender.send("/noteon", 3)
 def on_mouse_release(data):
     print("OFF")
-    sender.send("/noteoff", 2)
+    sender.send("/noteoff", 3)
 ctx.add_callback("mouse_press", on_mouse_press)
 ctx.add_callback("mouse_release", on_mouse_release)
 ctx.start(lambda: True)
