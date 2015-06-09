22d7f178a39786a80761805434a68a26db4740df
brianhouse <brian.house@gmail.com>
2015-06-09 18:48:23 -0400

    added script for computer control

---
+++ main_keyer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os, sys, time, random, threading, queue, json
+from housepy import osc, config, log, animation
+
+osc.verbose = False
+
+sender = osc.Sender(config['oscpin'], 23232)
+
+time.sleep(1)
+
+ctx = animation.Context(100, 100)
+def on_mouse_press(data):
+    print("ON")
+    sender.send("/noteon", 2)
+def on_mouse_release(data):
+    print("OFF")
+    sender.send("/noteoff", 2)
+ctx.add_callback("mouse_press", on_mouse_press)
+ctx.add_callback("mouse_release", on_mouse_release)
+ctx.start(lambda: True)
+
